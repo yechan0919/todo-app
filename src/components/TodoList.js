ff57a86854449abdf98d0fdf4a9c3759dda1f2b2
dpcks0919 <dpcks0919@gmail.com>
2023-12-13 11:26:47 +0900

    todo-app

---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TodoListItem from "./TodoListItem";
+import './TodoList.scss'
+
+const TodoList = ( {todo, onRemove, onToggle} ) => {
+    return (
+        <div className={"TodoList"}>
+            {todo.map( todo => (
+                <TodoListItem todo={todo} key={todo.id} onRemove ={onRemove} onToggle={onToggle}/>
+            ))}
+        </div>
+    );
+};
+
+export default TodoList;
